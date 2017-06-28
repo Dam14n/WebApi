b69c68b97b66b65dc7e85f8fd1db9b7585a37844
Damian Jorda <djorda@quadiontech.com>
2017-06-28 11:52:59 -0300

    Added DivisionService.cs

---
+++ WebService/BaseService.cs
@@ -0,0 +1,11 @@
+﻿using DataLayer;
+
+namespace WebService
+{
+	public class BaseService
+	{
+		private readonly ModelContext _context = new ModelContext();
+
+		protected ModelContext GetContext { get; set; }
+	}
+}
---
+++ WebService/DivisionService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataLayer;
+using Model;
+
+namespace WebService
+{
+	public class DivisionService
+	{
+		private readonly ModelContext _context = new ModelContext();
+
+		public List<Division> GetAll()
+		{
+			return _context.Divisions.ToList();
+		}
+
+		public Division GetDivision(int id)
+		{
+			return _context.Divisions.Find(id);
+		}
+	}
+}
